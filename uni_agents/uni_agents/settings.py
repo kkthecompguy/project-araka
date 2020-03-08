19cf21d601f4496b93775eef5e7cd9cfcad799e0
Kosam Omollo <hnkhosamomollo3@gmail.com>
2020-03-08 16:28:12 +0300

    allowed host

--- uni_agents/uni_agents/settings.py
+++ uni_agents/uni_agents/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k&ss341$&q5fun585+#8*mri^4^=j6upbv*8jgh&o4135f3hg&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'explore-edu.herokuapp.com']
 
 
 # Application definition
