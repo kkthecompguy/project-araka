111629ac014d775e93c4904ba9b3665e85f77ea9
Kosam Omollo <hnkhosamomollo3@gmail.com>
2020-03-08 10:18:27 +0300

    created agents profile

--- uni_agents/clients/models.py
+++ uni_agents/clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class University(models.Model):
@@ -7,13 +8,32 @@ class University(models.Model):
   city = models.CharField(max_length=100, null=True)
   state = models.CharField(max_length=100, null=True)
   postal = models.CharField(max_length=100, null=True)
+  country = models.CharField(max_length=100, null=True)
   website = models.CharField(max_length=100, null=True)
   email = models.EmailField(max_length=100, null=True)
   description = models.CharField(max_length=500, null=True)
+  isono = models.CharField(max_length=100, null=True)
   representative = models.CharField(max_length=100, null=True)
-  postion = models.CharField(max_length=100, null=True)
+  position = models.CharField(max_length=100, null=True)
   phone = models.CharField(max_length=100, null=True)
+  repemail = models.EmailField(max_length=100, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.name
+
+
+class Agent(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+  bio = models.CharField(max_length=200, null=True)
+  nationality = models.CharField(max_length=100, null=True)
+  national_id = models.CharField(max_length=20, null=True)
+  location = models.CharField(max_length=100, null=True)
+  birth_date = models.DateField(null=True)
+  phone = models.CharField(max_length=100, null=True)
+  profile_pic = models.ImageField(default="default_profile.jpg", null=True, blank=True)
+  is_active = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.user.first_name
--- uni_agents/clients/views.py
+++ uni_agents/clients/views.py
@@ -1,18 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import University, Agent
+from .forms import UniversityForm, CreateUserForm, ProfileForm
+
 
 # Create your views here.
 def registerAgent(request):
-  context = {
+  form = CreateUserForm()
 
+  if request.method == 'POST':
+    form = CreateUserForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      Agent.objects.create(
+        user = user
+      )
+      
+      return redirect('clients:login_agents')
+    else:
+      messages.info(request, 'The two passwords did not match')
+
+  context = {
+    'form': form
   }
   return render(request, 'clients/register-agent.html', context)
 
 
 def loginAgents(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      return redirect('clients:profile')
+    else:
+      messages.info(request, 'Invalid credentials')  
+
   context = {
 
   }
   return render(request, 'clients/login-agent.html', context)
+
+
+def logoutAgent(request):
+  logout(request)
+  return redirect('clients:login')
 
 
 def index(request):
@@ -40,16 +77,39 @@ def contact(request):
 
 
 def uniPortal(request):
+
+  if request.method == 'POST':
+    form = UniversityForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.info(request, 'Your information has been save successfully. Check your email after 24hours for further guidance')
+  else:
+    form = UniversityForm()
+
   context = {
-    'title': 'University Portal'
+    'form': form
   }
 
   return render(request, 'clients/university.html', context)
 
 
+@login_required(login_url='clients:login')
 def agents(request):
+  agent = request.user.agent
+
+  if request.method == 'POST':
+    form = ProfileForm(request.POST, request.FILES,  instance=agent)
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Your profile was updated successfully')
+    else:
+      messages.error(request, 'Please correct the error below')
+  else:
+    form = ProfileForm(instance=agent)
+  
+
   context = {
-    'title': 'Agents Portal'
+    'form': form
   }
 
   return render(request, 'clients/agents.html', context)
